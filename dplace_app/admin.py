4d5a9fa7d12b934a6586eea653c9db0f6bc3b4b8
Dan Leehr <dan.leehr@nescent.org>
2014-02-07 17:08:25 -0500

    Add Language model classes to admin

--- dplace_app/admin.py
+++ dplace_app/admin.py
@@ -9,3 +9,8 @@ admin.site.register(ISOCode, admin.GeoModelAdmin)
 admin.site.register(EAVariableDescription)
 admin.site.register(EAVariableCodeDescription)
 admin.site.register(EAVariableValue)
+
+admin.site.register(LanguageClass)
+admin.site.register(LanguageFamily)
+admin.site.register(LanguageClassification)
+admin.site.register(Language)
