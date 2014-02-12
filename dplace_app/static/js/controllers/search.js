f71bc1fe91669cad640637cfec4ac51e1686ca69
Dan Leehr <dan.leehr@nescent.org>
2014-02-12 16:17:08 -0500

    Converted selects to checkboxes for picking languages

--- dplace_app/static/js/controllers/search.js
+++ dplace_app/static/js/controllers/search.js
@@ -1,16 +1,42 @@
 function SearchCtrl($scope, $routeParams, LanguageClass) {
+    $scope.selections = [];
     $scope.families = LanguageClass.query({level: 1});
-    $scope.subfamilies = [];
-    $scope.subsubfamilies = [];
 
-    // functions to update subfamilies when family changes
-    $scope.setFamily = function(familyId) {
-        $scope.subsubfamilies = [];
-        $scope.subfamilies = LanguageClass.query({level:2, parent: familyId});
+    $scope.clearLanguages = function() {
+        $scope.selections = [];
+        $scope.families.forEach(function (family) {
+           family.subfamilies = [];
+            family.selected = false;
+        });
+    }
+
+    $scope.updateSelection = function(obj) {
+        if(obj.selected) {
+            $scope.selections.push(obj);
+        } else {
+            var index = $scope.selections.indexOf(obj);
+            if(index > -1) {
+                $scope.selections.splice(index, 1);
+            }
+        }
     };
 
-    $scope.setSubfamily= function(subFamilyId) {
-        $scope.subsubfamilies = LanguageClass.query({level:3, parent: subFamilyId});
+    $scope.updateFamily = function(family) {
+        if(family.selected) {
+            family.subfamilies = LanguageClass.query({level: 2, parent: family.id});
+        } else {
+            family.subfamilies = [];
+        }
+        $scope.updateSelection(family);
+    };
+
+    $scope.updateSubfamily = function(subfamily) {
+        if(subfamily.selected) {
+            subfamily.subsubfamilies = LanguageClass.query({level: 3, parent: subfamily.id});
+        } else {
+            subfamily.subsubfamilies = [];
+        }
+        $scope.updateSelection(subfamily);
     };
 
 }
