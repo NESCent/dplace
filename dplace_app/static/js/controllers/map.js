9cfa78d8ec62ab112de4860767ba6a5eac535e58
Dan Leehr <dan.leehr@nescent.org>
2014-02-26 10:05:18 -0500

    Add markers with popup labels
    Markers appear for societies after clicking 'Add Markers' button.  Need to refactor this to update when societies are updated.
    Markers were appearing in wrong place until c17387145295de80e9ab3acca67af7bb081fd636

--- dplace_app/static/js/controllers/map.js
+++ dplace_app/static/js/controllers/map.js
@@ -4,4 +4,32 @@ function MapCtrl($scope) {
             "http://vmap0.tiles.osgeo.org/wms/vmap0", {layers: 'basic'} );
     map.addLayer(wms);
     map.zoomToMaxExtent();
+    var markers = new OpenLayers.Layer.Markers("SocietyMarkers");
+    map.addLayer(markers);
+    $scope.map = map;
+    $scope.markers = markers;
+    var size = new OpenLayers.Size(21,25);
+    var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
+    $scope.icon = new OpenLayers.Icon('http://www.openlayers.org/dev/img/marker.png', size, offset);
+    $scope.addMarkers = function() {
+        $scope.markers.clearMarkers();
+        $scope.societies.forEach(function(society) {
+            var coordinates = society.location.coordinates;
+            // Add a marker for each point
+            var lonlat = OpenLayers.LonLat.fromArray(coordinates);
+            var marker = new OpenLayers.Marker(lonlat, $scope.icon.clone());
+            marker.events.register('mouseover', marker, function(evt) {
+                var popup = new OpenLayers.Popup.FramedCloud(society.name,
+                    marker.lonlat,          // lonlat
+                    null,                   // size
+                    society.name,           // html
+                    marker.icon,            // anchor
+                    false                   // closebox
+                );
+                $scope.map.addPopup(popup);
+                marker.events.register('mouseout', marker, function(evt) { popup.hide(); } )
+            });
+            $scope.markers.addMarker(marker);
+        });
+    };
 }
