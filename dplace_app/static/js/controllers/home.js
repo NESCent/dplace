a062d69cb289463ab3aa1d20d2d59febf57413e1
Dan Leehr <dan.leehr@nescent.org>
2014-03-07 17:06:53 -0500

    WIP on basic search form

--- dplace_app/static/js/controllers/home.js
+++ dplace_app/static/js/controllers/home.js
@@ -1,3 +1,16 @@
-function HomeCtrl($scope) {
+function HomeCtrl($scope, Variable) {
     $scope.setActive('home');
+    $scope.searchButtons = '';
+
+    $scope.buttonChanged = function() {
+        if($scope.searchButtons == 'geographic') {
+            $scope.regions = ['America','Albania','Zimbabwe','North America'];
+        } else if($scope.searchButtons == 'cultural') {
+            $scope.variables = Variable.query();
+        } else if($scope.searchButtons == 'environmental') {
+
+        } else if($scope.searchButtons == 'langauge') {
+
+        }
+    }
 }
