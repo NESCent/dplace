5d163702c5c120bfa9c4797a94fedfd6e0b264c8
Dan Leehr <dan.leehr@nescent.org>
2014-03-07 22:49:27 -0500

    WIP on cultural selectors and checkboxes

--- dplace_app/static/js/controllers/home.js
+++ dplace_app/static/js/controllers/home.js
@@ -1,16 +1,38 @@
-function HomeCtrl($scope, Variable) {
+function HomeCtrl($scope, Variable, CodeDescription) {
     $scope.setActive('home');
-    $scope.searchButtons = '';
+    $scope.selectedButton= '';
+    $scope.buttons = [
+        {value:'geographic', name:'Geographic'},
+        {value:'cultural', name:'Cultural Traits'},
+        {value:'environmental', name:'Environmental'},
+        {value:'language', name:'Language'}
+    ];
 
-    $scope.buttonChanged = function() {
-        if($scope.searchButtons == 'geographic') {
-            $scope.regions = ['America','Albania','Zimbabwe','North America'];
-        } else if($scope.searchButtons == 'cultural') {
-            $scope.variables = Variable.query();
-        } else if($scope.searchButtons == 'environmental') {
+    $scope.buttonChanged = function(selectedButton) {
+        $scope.selectedButton = selectedButton;
+        if(selectedButton == 'geographic') {
+            $scope.activateGeographic();
+        } else if(selectedButton == 'cultural') {
+            $scope.activateCultural();
+        } else if(selectedButton == 'environmental') {
 
-        } else if($scope.searchButtons == 'langauge') {
+        } else if(selectedButton == 'langauge') {
 
         }
+    };
+
+    // Geographic
+    $scope.activateGeographic = function() {
+        $scope.regions = ['America','Albania','Zimbabwe','North America'];
+    };
+
+    // Cultural Traits
+    $scope.activateCultural = function() {
+        $scope.traits = [{variables: Variable.query()}];
+    };
+    $scope.traitChanged = function(trait) {
+        trait.selectedCode = undefined;
+        trait.codes = CodeDescription.query({variable: trait.selectedVariableId });
     }
+
 }
