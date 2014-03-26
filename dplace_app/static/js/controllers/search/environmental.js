7971ea344f0aa8a28ec39085c327c8d8bf8e3ae2
Dan Leehr <dan.leehr@nescent.org>
2014-03-26 11:43:49 -0400

    Client implementation of find by environmental
    Builds JSON objects of selected filters for find_socieities

--- dplace_app/static/js/controllers/search/environmental.js
+++ dplace_app/static/js/controllers/search/environmental.js
@@ -1,12 +1,32 @@
-function EnvironmentalCtrl($scope, EnvironmentalVariable) {
+function EnvironmentalCtrl($scope, EnvironmentalVariable, FindSocieties) {
     $scope.environmentalVariables = EnvironmentalVariable.query();
     $scope.environmentalFilters = [
-        { key: 'inrange', name: 'between' },
-        { key: 'lt', name: 'less than'},
-        { key: 'gt', name: 'greater than'},
-        { key: 'outrange', name: 'outside'},
+        { operator: 'inrange', name: 'between' },
+        { operator: 'lt', name: 'less than'},
+        { operator: 'gt', name: 'greater than'},
+        { operator: 'outrange', name: 'outside'},
     ];
     $scope.environmental = {
+        selectedVariable: null,
+        vals: [],
         selectedFilter: $scope.environmentalFilters[0]
     };
+
+    $scope.getSelectedFilters = function() {
+        //environmental_vals: [{id: 1, operator: 'gt', params: [0.0]}, {id:3, operator 'inrange', params: [10.0,20.0] }
+        var environmental = $scope.environmental;
+        var filters = [{
+            id: environmental.selectedVariable.id,
+            operator: environmental.selectedFilter.operator,
+            params: environmental.vals
+        }];
+        return filters;
+    };
+
+    $scope.doSearch = function() {
+        var filters = $scope.getSelectedFilters();
+        $scope.results.societies = FindSocieties.find({ environmental_filters: filters});
+        // TODO: Activate the societies link
+    };
+
 }
