1e75a7525f32a0713c55bb69db05e22b10da1191
Dan Leehr <dan.leehr@nescent.org>
2014-05-05 16:19:38 -0400

    Sample data in language controller

--- dplace_app/static/js/controllers/search/language.js
+++ dplace_app/static/js/controllers/search/language.js
@@ -1,3 +1,10 @@
 function LanguageCtrl($scope) {
     // TODO: Fill in an actual language
+    $scope.language = {
+        items: [
+            { name: 'Language 1'},
+            { name: 'Language 2'},
+            { name: 'Language 3'}
+        ]
+    };
 }
