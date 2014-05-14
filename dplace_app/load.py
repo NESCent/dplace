11ecb390d65786936631a26effee1fe622d51073
Dan Leehr <dan.leehr@nescent.org>
2014-05-14 15:34:41 -0400

    Update load.py for geo regions

--- dplace_app/load.py
+++ dplace_app/load.py
@@ -6,6 +6,7 @@ from load.environmental import *
 from load.language import *
 from load.society_ea import *
 from load.society_binford import *
+from load.geographic import *
 
 LOAD_BY_ROW=('iso', 'env_vals',
              'langs', 'iso_lat_long',
@@ -13,51 +14,54 @@ LOAD_BY_ROW=('iso', 'env_vals',
              'bf_soc', 'bf_vars', 'bf_vals')
 
 def run(file_name=None, mode=None):
-    # read the csv file
-    with open(file_name, 'rb') as csvfile:
-        if mode in LOAD_BY_ROW:
-            csv_reader = csv.DictReader(csvfile)
-            for dict_row in csv_reader:
-                if mode == 'iso':
-                    load_isocode(dict_row)
-                elif mode == 'iso_lat_long':
-                    load_iso_lat_long(dict_row)
-                elif mode == 'ea_soc':
-                    load_ea_society(dict_row)
-                elif mode == 'env_vals':
-                    load_environmental(dict_row)
-                elif mode == 'ea_vars':
-                    load_ea_var(dict_row)
-                elif mode == 'ea_vals':
-                    load_ea_val(dict_row)
-                elif mode == 'langs':
-                    load_lang(dict_row)
-                elif mode == 'bf_soc':
-                    load_bf_society(dict_row)
-                elif mode == 'bf_vars':
-                    load_bf_var(dict_row)
-                elif mode == 'bf_vals':
-                    load_bf_val(dict_row)
-        elif mode == 'ea_codes':
-            load_ea_codes(csvfile)
-        elif mode == 'bf_codes':
-            load_bf_codes(csvfile)
-    if len(MISSING_CODES) > 0:
-        print "Missing ISO Codes:"
-        print '\n'.join(MISSING_CODES)
-    if mode == 'ea_vals':
-        # after loading values, populate society-level data from variable values
-        postprocess_ea_societies()
-    elif mode == 'bf_vals':
-        postprocess_binford_societies()
-    elif mode == 'env_vars':
-        create_environmental_variables()
-    elif mode == 'langs':
-        update_language_counts()
+    if mode == 'geo':
+        load_regions(file_name)
+    else:
+        # read the csv file
+        with open(file_name, 'rb') as csvfile:
+            if mode in LOAD_BY_ROW:
+                csv_reader = csv.DictReader(csvfile)
+                for dict_row in csv_reader:
+                    if mode == 'iso':
+                        load_isocode(dict_row)
+                    elif mode == 'iso_lat_long':
+                        load_iso_lat_long(dict_row)
+                    elif mode == 'ea_soc':
+                        load_ea_society(dict_row)
+                    elif mode == 'env_vals':
+                        load_environmental(dict_row)
+                    elif mode == 'ea_vars':
+                        load_ea_var(dict_row)
+                    elif mode == 'ea_vals':
+                        load_ea_val(dict_row)
+                    elif mode == 'langs':
+                        load_lang(dict_row)
+                    elif mode == 'bf_soc':
+                        load_bf_society(dict_row)
+                    elif mode == 'bf_vars':
+                        load_bf_var(dict_row)
+                    elif mode == 'bf_vals':
+                        load_bf_val(dict_row)
+            elif mode == 'ea_codes':
+                load_ea_codes(csvfile)
+            elif mode == 'bf_codes':
+                load_bf_codes(csvfile)
+        if len(MISSING_CODES) > 0:
+            print "Missing ISO Codes:"
+            print '\n'.join(MISSING_CODES)
+        if mode == 'ea_vals':
+            # after loading values, populate society-level data from variable values
+            postprocess_ea_societies()
+        elif mode == 'bf_vals':
+            postprocess_binford_societies()
+        elif mode == 'env_vars':
+            create_environmental_variables()
+        elif mode == 'langs':
+            update_language_counts()
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
-        print "\nUsage: %s csv_file mode" % sys.argv[0]
+        print "\nUsage: %s source_file mode" % sys.argv[0]
         print "You should run load_all_datasets.sh instead of this script directly."
         print
     else:
