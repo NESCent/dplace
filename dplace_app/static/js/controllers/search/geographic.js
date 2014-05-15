6591d03f5563f7a33bc914a4c44eb61a9f7d91d5
Dan Leehr <dan.leehr@nescent.org>
2014-05-15 10:44:09 -0400

    Use ajax typeahead instead of pre-fetching regions

--- dplace_app/static/js/controllers/search/geographic.js
+++ dplace_app/static/js/controllers/search/geographic.js
@@ -1,3 +1,11 @@
-function GeographicCtrl($scope, GeographicRegion) {
-    $scope.model.searchParams.regions = GeographicRegion.query();
+function GeographicCtrl($scope, GeographicRegion, $http, limitToFilter) {
+    /*
+        This uses $http instead of $resource because $http
+        returns a promise and not an initially empty array.
+     */
+    $scope.getRegions = function(regionName) {
+        return $http.get("/api/v1/geographic_regions?region_nam="+regionName).then(function(response){
+            return limitToFilter(response.data.results, 15);
+        });
+    };
 }
