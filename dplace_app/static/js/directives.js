8273b2b8b429d47ef92edfe2e51e13db6d7fe6fc
Dan Leehr <dan.leehr@nescent.org>
2014-05-15 15:14:52 -0400

    Replace OpenLayers map with jVectorMap
    in directive

--- dplace_app/static/js/directives.js
+++ dplace_app/static/js/directives.js
@@ -1,50 +1,28 @@
 angular.module('dplaceMapDirective', [])
     .directive('dplaceMap', function() {
         function link(scope, element, attrs) {
-            /* Temporarily Broken as OpenLayers is removed */
             element.append("<div id='mapdiv' style='width:1140px; height:480px;'></div>");
-            var map = new OpenLayers.Map('mapdiv');
-            var wms = new OpenLayers.Layer.WMS( "OpenLayers WMS",
-                    "http://vmap0.tiles.osgeo.org/wms/vmap0", {layers: 'basic'} );
-            map.addLayer(wms);
-            map.zoomToMaxExtent();
-            var markers = new OpenLayers.Layer.Markers("SocietyMarkers");
-            map.addLayer(markers);
-            scope.map = map;
-            scope.mapMarkers = markers;
-            var size = new OpenLayers.Size(21,25);
-            var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
-            scope.icon = new OpenLayers.Icon('http://www.openlayers.org/dev/img/marker.png', size, offset);
+            scope.map = $('#mapdiv').vectorMap({map: 'world_mill_en'}).vectorMap('get','mapObject');
             scope.addMarkers = function() {
-                scope.mapMarkers.clearMarkers();
+                scope.map.removeAllMarkers();
                 if(!scope.societies) {
                     return;
                 }
                 scope.societies.forEach(function(societyResult) {
                     var society = societyResult.society;
-                    var coordinates = society.location.coordinates;
                     // Add a marker for each point
-                    var lonlat = OpenLayers.LonLat.fromArray(coordinates);
-                    var marker = new OpenLayers.Marker(lonlat, scope.icon.clone());
-                    marker.events.register('mouseover', marker, function(evt) {
-                        var popup = new OpenLayers.Popup.FramedCloud(society.name,
-                            marker.lonlat,          // lonlat
-                            null,                   // size
-                            society.name,           // html
-                            marker.icon,            // anchor
-                            false                   // closebox
-                        );
-                        scope.map.addPopup(popup);
-                        marker.events.register('mouseout', marker, function(evt) { popup.hide(); } )
-                    });
-                    scope.mapMarkers.addMarker(marker);
+                    var marker = {latLng: society.location.coordinates.reverse(), name: society.name}
+                    scope.map.addMarker(society.id, marker);
                 });
             };
 
             // Update markers when societies change
             scope.$watchCollection('societies', function(oldvalue, newvalue) {
-                scope.map.zoomToMaxExtent();
                 scope.addMarkers();
+            });
+
+            scope.$on('mapTabActivated', function(event, args) {
+                scope.map.setSize();
             });
         }
 
