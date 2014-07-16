7be990927d7119578dde43a955ddc10fea6c240c
Dan Leehr <dan.leehr@nescent.org>
2014-07-16 12:23:52 -0400

    Placeholder value for download link

--- dplace_app/static/js/controllers/societies.js
+++ dplace_app/static/js/controllers/societies.js
@@ -2,5 +2,9 @@ function SocietiesCtrl($scope, $routeParams) {
     $scope.setActive('societies');
     $scope.resizeMap = function() {
         $scope.$broadcast('mapTabActivated');
-    }
+    };
+
+    $scope.generateDownloadLinks = function() {
+        $scope.csvDownloadLink = "#";
+    };
 }
