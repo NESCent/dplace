742e66856bf6f263b23384e5bc895a97e8db2013
SimonGreenhill <simon@simon.net.nz>
2014-09-01 21:25:45 +1000

    adding some rudimentary caching to go nice on the database

--- dplace_app/load/sources.py
+++ dplace_app/load/sources.py
@@ -1,11 +1,15 @@
 from dplace_app.models import Source
 
+_SOURCE_CACHE = {}
 def get_source(source='EA'):
+    if source in _SOURCE_CACHE:
+        return _SOURCE_CACHE[source]
+    
     if source == 'EA':
         try:
-            return Source.objects.get(year="1999", author="Murdock et al.")
+            o = Source.objects.get(year="1999", author="Murdock et al.")
         except Source.DoesNotExist:
-            return Source.objects.create(
+            o = Source.objects.create(
                 year="1999",
                 author="Murdock et al.",
                 reference="Murdock, G. P., R. Textor, H. Barry, III, D. R. White, J. P. Gray, and W. T. Divale. 1999. Ethnographic Atlas. World Cultures 10:24-136 (codebook)",
@@ -14,9 +18,9 @@ def get_source(source='EA'):
             )
     elif source == 'Binford':
         try:
-            return Source.objects.get(year="2001", author="Binford")
+            o = Source.objects.get(year="2001", author="Binford")
         except Source.DoesNotExist:
-            return Source.objects.create(
+            o = Source.objects.create(
                 year="2001",
                 author="Binford",
                 reference="Binford, L. 2001. Constructing Frames of Reference: An Analytical Method for Archaeological Theory Building Using Hunter-gatherer and Environmental Data Sets. University of California Press",
@@ -25,3 +29,5 @@ def get_source(source='EA'):
             )
     else:
         raise ValueError("Unknown Source: %s" % source)
+    _SOURCE_CACHE[source] = o
+    return o
