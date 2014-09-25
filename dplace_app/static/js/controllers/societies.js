690d5eb83e197edbd2ad4126eb5cb33a9d3f8096
Dan Leehr <dan.leehr@nescent.org>
2014-09-25 14:59:46 -0400

    Constructing CSV download URL from query

--- dplace_app/static/js/controllers/societies.js
+++ dplace_app/static/js/controllers/societies.js
@@ -6,6 +6,11 @@ function SocietiesCtrl($scope, searchModelService) {
     };
 
     $scope.generateDownloadLinks = function() {
-        $scope.csvDownloadLink = "#";
+        // queryObject is the in-memory JS object representing the chosen search options
+        var queryObject = searchModelService.getModel().getQuery();
+        // the CSV download endpoint is a GET URL, so we must send the query object as a string.
+        var queryString = encodeURIComponent(JSON.stringify(queryObject));
+        // format (csv/svg/etc) should be an argument, and change the endpoint to /api/v1/download
+        $scope.csvDownloadLink = "/api/v1/csv_download?query=" + queryString;
     };
 }
