b14330d05bcdb0dc96cf83793f32b95c9a08ceb5
Stef <stefelisabeth@gmail.com>
2014-10-20 13:22:51 +0800

    fixing error checking for environmental search

--- dplace_app/static/js/controllers/search/environmental.js
+++ dplace_app/static/js/controllers/search/environmental.js
@@ -25,7 +25,6 @@ function EnvironmentalCtrl($scope, searchModelService) {
         }
     };
     $scope.doSearch = function() {
-		$scope.submitted = true;
         var filters = getSelectedFilters();
         $scope.updateSearchQuery({ environmental_filters: filters });
         $scope.searchSocieties();
