050dd8a88a072807672cc0fa3c3e3f4a143c2790
Stef <stefelisabeth@gmail.com>
2014-11-13 01:53:36 +0800

    Populate newick_string field with newick string from language tree file

--- dplace_app/load/tree.py
+++ dplace_app/load/tree.py
@@ -1,3 +1,4 @@
+import re
 from os.path import basename
 from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 from django.core.files import File
@@ -33,6 +34,14 @@ def load_tree(file_name):
             tree.save()
     # now add languages to the tree
     reader = NexusReader(file_name)
+    #Remove '[&R]' from newick string
+    newick = re.sub(r'\[.*?\]', '', reader.trees.trees[0])
+    print "Formatting newick string %s" % (newick)
+    try:
+        newick = newick[newick.index('=')+1:]
+    except ValueError:
+        newick = newick
+    tree.newick_string = str(newick)
     for taxon_name in reader.trees.taxa:
         language = get_language(taxon_name)
         if language is not None:
