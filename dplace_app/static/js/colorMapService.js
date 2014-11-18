884d366afcfae0bbd3446cedd4e746b5cb5a21d4
Stef <stefelisabeth@gmail.com>
2014-11-18 09:45:29 +0800

    coloring markers based on trait value

--- dplace_app/static/js/colorMapService.js
+++ dplace_app/static/js/colorMapService.js
@@ -4,12 +4,29 @@ function ColorMapService() {
         return 'hsl(' + hue + ',100%,50%)';
     }
 
-    this.generateColorMap = function(keys) {
+    this.generateColorMap = function(societies, query) {
+        var colors = {};
+        for (var i = 0; i < societies.length; i++) {
+            var society = societies[i];
+            var id = society.society.id;
+            if (society.variable_coded_values.length > 0) {
+                var value = society.variable_coded_values[0].coded_value;
+                var count = query.variable_codes.length;
+                var color = mapColor(value, count);
+                colors[id] = color;
+            }
+        }
+        return colors;
+    
+    };
+    
+    
+    /*this.generateColorMap = function(keys) {
         var colors = {};
         for(var i=0;i<keys.length;i++) {
             var color = mapColor(i, keys.length);
             colors[keys[i]] = color;
         }
         return colors;
-    };
+    };*/
 }
