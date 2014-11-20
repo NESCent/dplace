308046c691a8619ac435b29d4ad92edf212814ba
Stef <stefelisabeth@gmail.com>
2014-11-20 18:38:42 +0800

    Fixing environmental variable issue

--- dplace_app/static/js/colorMapService.js
+++ dplace_app/static/js/colorMapService.js
@@ -7,27 +7,27 @@ function ColorMapService() {
     this.generateColorMap = function(societies, query, var_id) {
         var colors = {};
         if ('environmental_filters' in query) {
-        //not sure if this calculation should go here
-        //since we can only search for one environmental value at a time, using environmental_values[0] works fine
-            var extractedValues = societies.map(function(society) { return society.environmental_values[0].value; });
-            var min_value = Math.min.apply(null, extractedValues);
-            var max_value = Math.max.apply(null, extractedValues);
-            var range = max_value - min_value;
-            for (var i = 0; i < societies.length; i++) {
-                var society = societies[i];
-                var id = society.society.id;
-                var value = society.environmental_values[0].value; 
-                var color = mapColor(value, range);
-                colors[id] = color;
+            if (query.environmental_filters[0].id == var_id) {
+                var extractedValues = societies.map(function(society) { return society.environmental_values[0].value; });
+                var min_value = Math.min.apply(null, extractedValues);
+                var max_value = Math.max.apply(null, extractedValues);
+                var range = max_value - min_value;
+                for (var i = 0; i < societies.length; i++) {
+                    var society = societies[i];
+                    var id = society.society.id;
+                    var value = society.environmental_values[0].value;
+                    var color = mapColor(value, range);
+                    colors[id] = color;
+                }
             }
         } 
         
         if ('variable_codes' in query) {
+            console.log(query);
             var numCodes = 0;
             for (var i = 0; i < query.variable_codes.length; i++) {
                 if (query.variable_codes[i].variable == var_id) numCodes++;
-            }
-                        
+            }  
             for (var i = 0; i < societies.length; i++) {
                 var society = societies[i];
                 var id = society.society.id;
