922a157514744c95065714b10e2af1ce4d6b79b7
Stef <stefelisabeth@gmail.com>
2014-11-21 12:29:07 +0800

    making order of markers on phylogeny consistent

--- dplace_app/static/js/directives.js
+++ dplace_app/static/js/directives.js
@@ -65,6 +65,36 @@ angular.module('languagePhylogenyDirective', [])
                 //keeps track of how many variables are mapped on the tree
                 //sometimes variables are searched for, but do not appear on any trees
                 var keyCount = 0; 
+                translate = 0;
+                for (var key in scope.code_ids) {
+                    keyCount++;
+                    scope.results.societies.forEach(function(society) {
+                        var selected = node.filter(function(d) {
+                            return d.name == society.society.iso_code;
+                        });
+                        
+                        if (society.variable_coded_values.length > 0) {
+                            for (var i = 0; i < society.variable_coded_values.length; i++) {
+                                if (society.variable_coded_values[i].variable == key) {
+                                        selected.append("svg:circle")
+                                            .attr("r", 4.5)
+                                            .attr("stroke", "#000")
+                                            .attr("stroke-width", "0.5")
+                                            .attr("transform", "translate("+translate+", 0)")
+                                            .attr("fill", function(n) {
+                                                value = society.variable_coded_values[i].coded_value;
+                                                hue = value * 240 / scope.code_ids[society.variable_coded_values[i].variable];
+                                                return 'hsl('+hue+',100%, 50%)';
+                                            });                        
+                                        if (i >= keyCount) keyCount = i+1;
+                                        continue;
+                                } 
+                                keyCount--;
+                            }
+                        }
+                    });
+                    translate += 20;
+                }
                 
                 scope.results.societies.forEach(function(society) {
                     var selected = node.filter(function(d) {
@@ -72,8 +102,8 @@ angular.module('languagePhylogenyDirective', [])
                     });
                     
                     //append the circles
-                    translate = 0;
-                    if (society.variable_coded_values.length > 0) {
+                    //translate = 0;
+                   /* if (society.variable_coded_values.length > 0) {
                         for (var i = 0; i < society.variable_coded_values.length; i++) {
                             selected.append("svg:circle")
                                 .attr("r", 4.5)
@@ -88,7 +118,7 @@ angular.module('languagePhylogenyDirective', [])
                             translate += 20;
                             if (i >= keyCount) keyCount = i+1;
                         }
-                    }
+                    }*/
                     
                     if (society.environmental_values.length > 0) {
                         selected.append("svg:circle")
@@ -105,8 +135,8 @@ angular.module('languagePhylogenyDirective', [])
                     }
                     
                     //lastly, append the text
-                    if (keyCount * 20 != translate) translate = keyCount * 20;
-                    if (society.environmental_values.length > 0) translate += 20;
+                    //if (keyCount * 20 != translate) translate = keyCount * 20;
+                    //if (society.environmental_values.length > 0) translate += 20;
                         selected.append("svg:text") 
                             .attr("dx", translate-10)
                             .attr("dy", 4)
