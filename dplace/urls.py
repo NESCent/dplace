eccb1cf7d86241e615d9b19314d1ad79146fc6b6
SimonGreenhill <simon@simon.net.nz>
2016-02-09 15:04:03 +1100

    removing admin from urls since we are not using it

--- dplace/urls.py
+++ dplace/urls.py
@@ -1,8 +1,5 @@
 from django.conf.urls import include, url
 
-from django.contrib.gis import admin
-admin.autodiscover()
-
 urlpatterns = [
     url(r'^', include('dplace_app.urls')),
     url(r'^api/', include('rest_framework.urls', namespace='rest_framework')),
