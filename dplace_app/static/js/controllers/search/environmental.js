533999061873167fadb5819c637f00b2072a905a
Stef <stefelisabeth@gmail.com>
2016-02-19 10:58:16 +1300

    Fixing variableChanged function

--- dplace_app/static/js/controllers/search/environmental.js
+++ dplace_app/static/js/controllers/search/environmental.js
@@ -27,12 +27,12 @@ function EnvironmentalCtrl($scope, searchModelService, EnvironmentalVariable, En
     $scope.variableChanged = function(variable) {
         if(variable.selectedVariable != null) {
             $scope.environmentalData.badgeValue += 1;
-        } 
-
-        variable.EnvironmentalForm.$setPristine();
-        $scope.values = MinAndMax.query({query: {environmental_id: variable.selectedVariable.id}});
-        $scope.filterChanged(variable);
+            variable.EnvironmentalForm.$setPristine();
+            $scope.values = MinAndMax.query({query: {environmental_id: variable.selectedVariable.id}});
+            $scope.filterChanged(variable);
+        }
     };
+    
     
     $scope.filterChanged = function(variable) {
         if (variable.EnvironmentalForm.$dirty && variable.selectedFilter.operator != 'all') return;
