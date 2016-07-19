e3be37f353ac415d024276574936c1fdff2c669b
Stef <stefelisabeth@gmail.com>
2016-07-19 15:45:51 +1200

    URLs

--- dplace_app/static/js/controllers/app.js
+++ dplace_app/static/js/controllers/app.js
@@ -21,22 +21,8 @@ function AppCtrl($scope, $location, $http, searchModelService) {
         $location.path('/societies');
         var queryObject = $scope.model.getQuery();
         for (var key in queryObject) {
-            if (key == 'c') {
-                queryString = "["
-                for (var v = 0; v < queryObject[key].length; v++) {
-                    if (queryObject[key][v].id)
-                        queryString += queryObject[key][v].variable + '-' + queryObject[key][v].id;
-                    else {
-                        queryString += queryObject[key][v].variable + '-' + queryObject[key][v].min + '-' + queryObject[key][v].max;
-                    }
-                    if (v < queryObject[key].length-1)
-                        queryString += ',';
-                }
-                queryString += "]"
-                $location.search(key, queryString);
-            } else {
-                $location.search(key, JSON.stringify(queryObject[key]));
-            }
+            $location.search(key, JSON.stringify(queryObject[key]));
+            
         }
     };
     
