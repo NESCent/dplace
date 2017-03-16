7b25271ecde5a390234aa542d7bd9cd1dd30ad15
SimonGreenhill <simon@simon.net.nz>
2017-03-16 15:01:02 +0100

    tidy

--- karma.conf.js
+++ karma.conf.js
@@ -7,15 +7,12 @@ module.exports = function(config) {
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
-
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine-jquery','jasmine'],
 
-
     // list of files / patterns to load in the browser
     files: [
-        //'dplace_app/static/bower_components/jquery/src/jquery.js',
         'dplace_app/static/bower_components/angular/angular.min.js',
         'dplace_app/static/bower_components/angular-resource/angular-resource.min.js',
         'dplace_app/static/bower_components/angular-route/angular-route.min.js',
@@ -32,15 +29,11 @@ module.exports = function(config) {
         'dplace_app/static/bower_components/angular-ui-bootstrap-bower/ui-bootstrap-tpls.min.js',
         'dplace_app/static/js/controllers/search/*.js',
         'dplace_app/tests/*.js',
-        
         {pattern: 'dplace_app/tests/data/*.json', watched: true, served: true, included: false}
     ],
 
-
     // list of files to exclude
-    exclude: [
-    ],
-
+    exclude: [],
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
@@ -57,28 +50,22 @@ module.exports = function(config) {
         ]
     },
 
-
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
+    colors: false,
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['PhantomJS'],
-
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
