0ddb79fce6805d71eb905b7f8d2143a4ab9c5031
xrotwang <xrotwang@googlemail.com>
2017-03-17 21:13:21 +0100

    adapted phylogeny loading to new dplace-data layout

--- dplace_app/api_views.py
+++ dplace_app/api_views.py
@@ -155,7 +155,7 @@ class TreeResultsSetPagination(PageNumberPagination):
     Since trees may have *many* languages, which are serialized as well, we limit the
     page size to just 1.
     """
-    page_size = 1
+    page_size = 3
     page_size_query_param = 'page_size'
     max_page_size = 10
 
